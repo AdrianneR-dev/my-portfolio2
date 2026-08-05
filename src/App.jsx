8bd5898c69399cf3506ecfbf62df07a7c37e4e9a
AdrianneR-dev <adrhodes04@gmail.com>
2026-08-05 07:58:53 -0500

    In App.jsx line 19 route path is "/" instead of "/Home"

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     <BrowserRouter>
     <Navbar/>
     <Routes>
-      <Route path="/Home" element={<Home />}/>
+      <Route path="/" element={<Home />}/>
       <Route path="/Experience" element={<Experience />}/>
       <Route path="/Projects" element={<Projects />}/>
       <Route path="/Contact" element={<Contact />}/>
