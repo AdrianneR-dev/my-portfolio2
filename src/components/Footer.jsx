51d463da6ef60d7ce82eb2c996fc3fffa8f9c150
AdrianneR-dev <adrhodes04@gmail.com>
2026-08-07 07:09:18 -0500

    Adding space between the footer links with a <p> tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ function Footer() {
         <div>
             <h3>Links</h3>
             <a href="www.linkedin.com/in/adrianne-r-340801143">LinkedIn</a>
-            <a href="https://github.com/AdrianneR-dev">     GitHub</a>
+            <p></p>
+            <a href="https://github.com/AdrianneR-dev">GitHub</a>
         </div>
     )
 }
